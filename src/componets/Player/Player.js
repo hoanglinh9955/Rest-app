cd92d78d4416027dfb08e502d27d9c40fc56ed22
hoang linh <hoanglinh9955@gmail.com>
2023-02-02 11:13:03 +0700

    add map function

---
+++ src/componets/Player/Player.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Players } from "../../shared/ListOfPlayer";
+export default class Player extends Component {
+  render() {
+    return (
+      <div className="container">
+        {Players.map((player) => (
+          <div className="column">
+            <div className="card">
+              <img src={player.img} alt="?" />
+              <h3>{player.name}</h3>
+              <p className="title">{player.club}</p>
+              <p>
+                <button>Detail</button>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
